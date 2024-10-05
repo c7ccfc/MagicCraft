40b345aad7e301ba0f7d3b71ee798a4b5017951a
embulan <ebulan2023@gmail.com>
2024-10-05 12:34:23 -0400

    API Key Holder
    Initial Commit test too - added the API Key Holder and made an API key object

---
+++ Assets/AICalling/APIKeyHolding.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "APIKeyHolder", menuName = "ScriptableObjects/APIKeyHolder", order = 1)]
+public class APIKeyHolding : ScriptableObject
+{
+    [SerializeField] private string apiKey;
+
+    // Getter to access the API key
+    public string GetApiKey()
+    {
+        return apiKey;
+    }
+
+    // Setter to update the API key (optional, for future use)
+    public void SetApiKey(string newKey)
+    {
+        apiKey = newKey;
+    }
+}
