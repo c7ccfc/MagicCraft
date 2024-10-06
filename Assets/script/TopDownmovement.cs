a85bc56bbab368d2e7801f5816df2122fc80682f
Justin Park <justin.park@duke.edu>
2024-10-06 15:02:40 -0400

    make character movement smoother

--- Assets/script/TopDownmovement.cs
+++ Assets/script/TopDownmovement.cs
@@ -8,34 +8,53 @@ public class TopDown : MonoBehaviour
     public GameObject stabPrefab;
     public GameObject AOEPrefab;
     public GameObject ProjectilePrefab;
-    
+    private Animator animator;
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-       Vector3 pos = transform.position;
-       if(Input.GetKey("w"))
-       {
-          pos.y+= speed*Time.deltaTime;
-       }
-       if(Input.GetKey("s"))
-       {
-          pos.y-= speed*Time.deltaTime;
-       }
-       if(Input.GetKey("d"))
-       {
-          pos.x+= speed*Time.deltaTime;
-       }
-       if(Input.GetKey("a"))
-       {
-          pos.x-= speed*Time.deltaTime;
-       }
-       transform.position = pos;
+        Vector3 pos = transform.position;
+        bool isMoving = false;
+
+        // Movement Input
+        if (Input.GetKey("w"))
+        {
+            pos.y += speed * Time.deltaTime;
+            isMoving = true;
+        }
+        if (Input.GetKey("s"))
+        {
+            pos.y -= speed * Time.deltaTime;
+            isMoving = true;
+        }
+        if (Input.GetKey("d"))
+        {
+            pos.x += speed * Time.deltaTime;
+            isMoving = true;
+            // Flip character to face right
+            transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        }
+        if (Input.GetKey("a"))
+        {
+            pos.x -= speed * Time.deltaTime;
+            isMoving = true;
+            // Flip character to face left
+            transform.localScale = new Vector3(-Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);
+        }
+
+        // Apply the new position
+        transform.position = pos;
+
+        // Update Animator
+        animator.SetBool("isWalking", isMoving);
     }
 
     public void Stab(float damage)
